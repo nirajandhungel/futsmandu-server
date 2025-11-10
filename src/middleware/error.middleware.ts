1b5a41acb058e32869de53dc24b2b975e3331f52
nirajandhungel <subashdhungel555@gmail.com>
2025-11-10 19:26:11 +0545

    error(import): fix import type error and typo

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -1,7 +1,7 @@
-import {Request, Response, NextFunction} from 'express';
-import {HTTP_STATUS,ERROR_MESSAGES} from '../config/constants.ts';
-import logger from '../utils/logger.ts';
-import {createResponse} from '../utils/helpers.ts'
+import type {Request, Response, NextFunction} from 'express';
+import {HTTP_STATUS,ERROR_MESSAGES} from '../config/constants.js';
+import logger from '../utils/logger.js';
+import {createResponse} from '../utils/helpers.js'
 
 export class AppError extends Error{
     statusCode: number;
@@ -25,5 +25,5 @@ export const errorHandler=(
     next: NextFunction
 ):void=>{
     err.statusCode = err.statusCode || HTTP_STATUS.INTERNAL_SERVER_ERROR;
-    err.message = err.message || ERROR_MESSAGES.INTERNAL_SERVER_ERROR;
+    err.message = err.message || ERROR_MESSAGES.INTERNAL_ERROR;
 }
