66fbe2fcb097462e8e22dbe8251510e0f3947c67
nirajandhungel <subashdhungel555@gmail.com>
2025-11-14 16:07:25 +0545

    feat(routes): add auth routes with validation, rate limiting, and middleware integration

---
+++ src/routes/auth.routes.ts
@@ -0,0 +1,41 @@
+import {Router } from 'express';
+
+import * as authController from '../controllers/auth.controller.js';
+import {authenticate} from '../middleware/auth.middleware.js';
+import { validateRequest, validationSchemas } from '../middleware/validation.middleware.js';
+
+import {authLimiter} from '../middleware/rateLimit.middleware.js';  
+
+const router = Router();
+
+// User Registration
+router.post(
+    '/register',
+    // authLimiter,
+    validateRequest(validationSchemas.register),
+    authController.register
+);
+
+// User Login
+router.post(
+    '/login',
+    authLimiter,
+    validateRequest(validationSchemas.login),
+    authController.login
+);
+
+// logout user
+router.post(
+    '/logout',
+    authenticate,
+    authController.logout
+);
+
+// rfresh token
+router.post(
+    '/refresh-token',
+    authLimiter,
+    authController.refreshToken
+);
+
+export default router;
