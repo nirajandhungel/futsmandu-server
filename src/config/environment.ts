fcf02a8eb0df7e3077471d11340d6fda02bdd91d
nirajandhungel <subashdhungel555@gmail.com>
2025-11-20 18:49:57 +0545

    feat(environment): add Cloudinary configuration to environment schema

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -26,6 +26,11 @@ const envSchema = Joi.object({
     APP_NAME:Joi.string().default('FUTSMANDU'),
     APP_VERSION:Joi.string().default('1.0.0'),
     CLIENT_URL:Joi.string().uri().required(),
+    CLOUDINARY_CLOUD_NAME:Joi.string().required(),
+    CLOUDINARY_API_KEY:Joi.string().required(),
+    CLOUDINARY_API_SECRET:Joi.string().required(),
+    CLOUDINARY_UPLOAD_PRESET:Joi.string().optional(),
+    CLOUDINARY_BASE_FOLDER:Joi.string().default('futsmandu'),
 }).unknown();
 
 const {error, value:envVars} = envSchema.validate(process.env);
@@ -68,6 +73,14 @@ export const config={
         name:envVars.APP_NAME,
         version:envVars.APP_VERSION,
         clientUrl:envVars.CLIENT_URL,
+    },
+
+    cloudinary:{
+        cloudName:envVars.CLOUDINARY_CLOUD_NAME,
+        apiKey:envVars.CLOUDINARY_API_KEY,
+        apiSecret:envVars.CLOUDINARY_API_SECRET,
+        uploadPreset:envVars.CLOUDINARY_UPLOAD_PRESET,
+        baseFolder:envVars.CLOUDINARY_BASE_FOLDER,
     },
 
 }
